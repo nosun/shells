2b49fb4cba2b0ef77a8d87865618894acb4256e9
nosun <nosun2008@126.com>
2017-05-25 15:27:46 +0800

    add ubuntu install shell

---
+++ ubuntu/init.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# soft update
+apt-get update
+apt-get upgrade -y
+
+# locale setting
+locale-gen zh_CN.UTF-8
+
+# install software
+apt-get install vim -y
+apt-get install sysv-rc-conf -y
+apt-get install iptables-persistent -y
+apt-get install git-core -y
+apt-get install zsh -y
+apt-get install libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl make build-essential -y
+
+# oh my zsh install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
---
+++ ubuntu/install_openresty.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+OPENRESTY_VER=1.11.2.3
+
+cd /usr/local/src
+
+wget https://openresty.org/download/openresty-${OPENRESTY_VER}.tar.gz
+
+tar -zxvf openresty-${OPENRESTY_VER}.tar.gz
+
+cd /usr/local/src/openresty-${OPENRESTY_VER}
+
+./configure --prefix=/usr/local/openresty \
+  --http-log-path=/var/log/openresty/access.log \
+  --error-log-path=/var/log/openresty/error.log \
+  --http-client-body-temp-path=/var/cache/openresty/client_temp \
+  --http-proxy-temp-path=/var/cache/openresty/proxy_temp \
+  --http-fastcgi-temp-path=/var/cache/openresty/fastcgi_temp \
+  --http-uwsgi-temp-path=/var/cache/openresty/uwsgi_temp \
+  --http-scgi-temp-path=/var/cache/openresty/scgi_temp \
+  --pid-path=/var/run/openresty.pid \
+  --with-http_realip_module \
+  --with-http_stub_status_module \
+  --with-http_gzip_static_module \
+  --with-http_ssl_module \
+  --with-cc-opt='-O2 -g' \
+  --with-luajit
+
+make && make install
+
+mkdir -P /data/logs/nginx
+mkdir -p /run/nginx
+chown -R www-data.www-data /data/
+chown -R www-data.www-data /run/nginx
+cp etc/init.d/nginx /etc/init.d/
+chmod +x /etc/init.d/nginx
+
