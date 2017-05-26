a2feb768d4d614e88e339d4a39d2f1101a45eee3
nosun <nosun2008@126.com>
2017-05-26 17:40:49 +0800

    add sudo

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 # soft update
-apt-get update -y
-DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+sudo apt-get update -y
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 # locale setting
-locale-gen zh_CN.UTF-8
+sudo locale-gen zh_CN.UTF-8
 
 # install software
-apt-get install vim -y
-apt-get install sysv-rc-conf -y
-apt-get install iptables-persistent -y
-apt-get install git-core -y
-apt-get install zsh -y
+sudo apt-get install vim -y
+sudo apt-get install sysv-rc-conf -y
+sudo apt-get install iptables-persistent -y
+sudo apt-get install git-core -y
+sudo apt-get install zsh -y
 
 # oh my zsh install
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
