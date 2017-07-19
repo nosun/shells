a074e38e2d30a8678306e95580b6e4e59048f9d1
nosun <nosun2008@126.com>
2017-07-19 19:43:22 +0800

    update

--- ubuntu/install_thumbor_server.sh
+++ ubuntu/install_thumbor_server.sh
@@ -17,3 +17,7 @@ sudo apt-get install libmagickwand-dev -y
 sudo apt-get install luarocks -y
 sudo luarocks install magick
 
+## cp config file
+## 此处需要注意的是版本软件的版本，本版本只适用于当下，如果不是，请比对更新
+cp ./etc/supervisor/supervisord.conf /etc/
+cp ./etc/thumbor/thumbor.conf /etc/
