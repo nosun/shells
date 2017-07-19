fc13b6f51aecfe4483f189ea94ee9736dd95fcdb
root <root@ip-172-31-6-18.us-west-2.compute.internal>
2017-07-19 06:24:17 +0000

    add install thumbor

---
+++ ubuntu/install_thumbor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get install libcurl4-openssl-dev -y
+apt-get install libjpeg-dev -y
+apt-get install python-dev -y
+apt-get install python-pip -y
+pip install --upgrade pip
+pip install thumbor
