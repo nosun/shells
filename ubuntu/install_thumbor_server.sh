89d13b3b23d8b824542695d738d975ab03b38b5e
root <root@ip-172-31-1-23.us-west-2.compute.internal>
2017-07-19 10:54:24 +0000

    update

--- ubuntu/install_thumbor_server.sh
+++ ubuntu/install_thumbor_server.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# install thumbor
 apt-get install libcurl4-openssl-dev -y
 apt-get install libjpeg-dev -y
 apt-get install python-dev -y
 apt-get install python-pip -y
 pip install --upgrade pip
 pip install thumbor
+
+## install supervisord
+pip install supervisor
+
+## install magick
+
+sudo apt-get install libmagickwand-dev -y
+sudo apt-get install luarocks -y
+sudo luarocks install magick
+
