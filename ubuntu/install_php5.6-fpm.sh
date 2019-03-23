c737a42d501b436e0f798d04d3958043ddf1843f
nosun <nosun2008@126.com>
2019-03-23 19:20:11 +0800

    update

--- ubuntu/install_php5.6-fpm.sh
+++ ubuntu/install_php5.6-fpm.sh
@@ -3,5 +3,4 @@
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get update && apt-get upgrade -y
-sudo apt-get install php5.6 php5.6-fpm php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml -y
-sudo sysv-rc-conf php5.6-fpm  on
+apt-get install php5.6 php5.6-fpm php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml -y
---
+++ ubuntu/install_php7.2-fpm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo apt-get install php7.2-fpm php7.2 php7.2-cli php7.2-mbstring php7.2-mysql php7.2-xml php7.2-gd php7.2-mysql php-redis php7.2-curl php7.2-zip  -y
