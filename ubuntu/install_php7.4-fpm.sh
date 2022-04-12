0ba2282aa438c8e475120ef6d4735ee27244b38f
root <root@ip-172-31-12-7.us-west-2.compute.internal>
2022-04-12 09:31:58 +0000

    update

---
+++ ubuntu/install_php7.4-fpm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo apt-get install php7.4-fpm php7.4 php7.4-cli php7.4-bcmath php7.4-bz2 php7.4-mbstring php7.4-mysql php7.4-xml php7.4-gd php7.4-mysql php-redis php7.4-curl php7.4-zip  -y
