833c87f9697fad7a45eac0c417c151671b87897c
nosun <nosun2008@126.com>
2022-05-15 08:17:50 +0800

    update

---
+++ check/check_elasticsearch.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+action=$1
+ROOT=`pwd`
+SCRIPT=/etc/init.d/elasticsearch
+LOGPATH=${ROOT}/elastichsearch_check.log
+
+
+function do_start()
+{
+    get_pid
+    if [ "$pid" ] ;then
+       echo "already started"
+    else
+        echo start it
+        ${SCRIPT} start >> ${LOGPATH} 2>&1 &
+        echo $(date +%Y-%m-%di_%H:%M:%S)"restarted" >> ${LOGPATH}/queue.log
+    fi
+}
+
+function do_stop()
+{
+    get_pid
+    if [ "$pid" ] ;then
+       kill -15 $pid
+       echo "stopped the process"
+    else
+       echo "no process is running"
+    fi
+}
+
+function get_pid()
+{
+   pid=`ps -fe | grep "${SCRIPT}" | grep -v "grep" |awk -F " " '{print $2}'`
+}
+
+
+case $action in
+
+start)
+    do_start;
+;;
+
+stop)
+    do_stop;
+;;
+
+restart)
+    do_stop;
+    do_start;
+;;
+
+info)
+    get_pid;
+    if [ "$pid" ] ;then
+       echo "the process pid is "$pid
+    else
+       echo "no process is running"
+    fi
+;;
+*)
+    echo "bad args, you need input args in start,stop,restart"
+;;
+
+esac
--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -18,3 +18,17 @@ sudo apt-get install squid -y
 
 # oh my zsh install
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+## 修改 .zshrc 
+
+#ZSH_THEME="nosun"
+
+## 增加 theme 
+
+#local ret_status="%(?:%{$fg_bold[green]%}➜:%{$fg_bold[red]%}➜)"
+#PROMPT='%{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%}%m%{$reset_color%} ${ret_status}%{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%} % %{$reset_color%}'
+
+#ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
+#ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+#ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}➜W"
+#ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
